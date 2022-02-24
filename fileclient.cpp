e0801fa3de98b7a96d519d2d586f9b79fc78736e
littleSub <r_yao911@outlook.com>
2022-02-24 01:58:06 -0500

    print checksum

--- fileclient.cpp
+++ fileclient.cpp
@@ -201,6 +201,7 @@ int main(int argc, char *argv[])
                 requestCheck = filename + "#" + originalchecksum;
                 sock->write(requestCheck.c_str(), strlen(requestCheck.c_str()) + 1);
                 readlen = sock->read(incomingMessage, sizeof(incomingMessage));
+                cout << "Original Checksum is: " << originalchecksum << endl;
                 if (readlen == 0)
                 {
                     c150debug->printf(C150APPLICATION, "Read zero length message, trying again");
