4378f0c7a3d7a988ff8afb99465699e580471ae4
littleSub <r_yao911@outlook.com>
2022-02-24 02:03:35 -0500

    sdf\

--- fileclient.cpp
+++ fileclient.cpp
@@ -200,7 +200,7 @@ int main(int argc, char *argv[])
                 string filename(sourceFile->d_name);
                 requestCheck = filename + "#" + originalchecksum;
                 sock->write(requestCheck.c_str(), strlen(requestCheck.c_str()) + 1);
-                printf("Original checksum is: %ss\n", originalchecksum);
+                printf("Original checksum is: %s\n", originalchecksum.c_str());
                 
                 if (readlen == 0)
                 {
