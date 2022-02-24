828f9ee09da47d9291e851360f30b241dcbd2aec
littleSub <r_yao911@outlook.com>
2022-02-23 21:58:16 -0500

    1

--- fileserver.cpp
+++ fileserver.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 
     unsigned char shaComputedHash[20]; // hash goes here
     // bool end2endCheck;                 // if the file is identical with the original one
-    string delim = '#'; //  the delimeter to spilt the incoming message
+    string delim = "#"; //  the delimeter to spilt the incoming message
     DIR *TARGET;
     // Unix descriptor for target
     vector<string> splitinput;
