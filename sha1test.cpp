7f3db1908a3b3381936f167b9390b4d93a25ceb0
littleSub <r_yao911@outlook.com>
2022-02-24 19:14:24 -0500

    stringbuffer

--- sha1test.cpp
+++ sha1test.cpp
@@ -1,19 +1,18 @@
-// 
+//
 //            sha1tstFIXED
 //
 //     Author: Noah Mendelsohn
 //
-//     Test programming showing use of computation of 
+//     Test programming showing use of computation of
 //     sha1 hash function.
 //
 //     NOTE: problems were discovered using the incremental
-//     version of the computation with SHA1_Update. This 
+//     version of the computation with SHA1_Update. This
 //     version, which computes the entire checksum at once,
 //     seems to be reliable (if less flexible).
 //
-//     Note: this must be linked with the g++ -lssl directive. 
+//     Note: this must be linked with the g++ -lssl directive.
 //
-
 
 #include <string>
 #include <cstdlib>
@@ -26,39 +25,38 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  int i, j;
-  ifstream *t;
-  stringstream *buffer;
-  string out;
-  ostringstream os;
+	int i, j;
+	ifstream *t;
+	stringstream *buffer;
+	string out;
+	char *stringbuffer;
 
-  unsigned char obuf[20];
-  
+	unsigned char obuf[20];
 
-  if (argc < 2) 	{
-		fprintf (stderr, "usage: %s file [file...]\n", argv[0]);
-		exit (1);
-  }
+	if (argc < 2)
+	{
+		fprintf(stderr, "usage: %s file [file...]\n", argv[0]);
+		exit(1);
+	}
 
-  for (j = 1; j < argc; ++j) {
-		printf ("SHA1 (\"%s\") = ", argv[j]);
+	for (j = 1; j < argc; ++j)
+	{
+		printf("SHA1 (\"%s\") = ", argv[j]);
 		t = new ifstream(argv[j]);
 		buffer = new stringstream;
 		*buffer << t->rdbuf();
-		SHA1((const unsigned char *)buffer->str().c_str(), 
-		     (buffer->str()).length(), obuf);
+		SHA1((const unsigned char *)buffer->str().c_str(),
+			 (buffer->str()).length(), obuf);
 		for (i = 0; i < 20; i++)
 		{
 			// printf ("%02x", (unsigned int) obuf[i]);
-			os<<obuf[i];
-			out += os.str();
+			sprintf(stringbuffer, "%02x", (unsigned int)obuf[i]);
+
 		}
-		printf(out.c_str());
-		printf ("\n");
+		printf(stringbuffer);
+		printf("\n");
 		delete t;
 		delete buffer;
-  }
-  return 0;
+	}
+	return 0;
 }
-
-
